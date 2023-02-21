211eacc958012eedbc0ff414fdc5667c92029f35
LAHIN <lahin@MACs-MacBook-Pro.local>
2023-02-21 01:29:07 +0000

    json added

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Portfolio.css";
 
 
 
 const Portfolio = () => {
+  const [projects, setProjects]= useState([]);
+  useEffect(()=>{
+    fetch('../../allprojects.json')
+    .then(reponse=>reponse.json())
+    .then(data=> setProjects(data));
+  }, [])
   return (
+
+
     <div className="portfolio-body">
       <div className="portfolio-text container">
         <h1>Portfolio</h1>
@@ -14,23 +22,29 @@ const Portfolio = () => {
         <h1>My Amazing Works</h1>
       </div>
 
-      <div className="container">
-        <div class="row container">
-          <div class="col-sm-6 mb-3 col-lg-4 mb-sm-0">
-    
-            <div class="card"  >
-            <img src="" class="card-img-top" alt="..." />
+      <div className="container   ">
+        <div class="row container   ">
+          <div class="col-sm-6 mb-3 col-lg-10 mb-sm-0 d-flex mx-auto  ">
+            {
+              projects.map(project=> (
+
+                <div class="card row-container   "  >
+
+              <img src={project.img} alt="" srcset="" />
+          
               <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">
-                  With supporting text below as a natural lead-in to additional
-                  content.
+                <h5 class="card-title">Website About: {project.title}</h5>
+                <p class="card-text"> Technologies Covered:
+                  {project.technologies}
                 </p>
-                <a href="#" class="btn link-btn">
+                <a href={project.website} target='_blank' class="btn link-btn">
                     Live Link
                 </a>
               </div>
             </div>
+
+              ))
+            }
             
           </div>
         </div>
